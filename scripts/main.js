18a89492a2f9f282fd795e9bcae826b6a8f4a9e1
Warden <samer.charaf3@gmail.com>
2023-11-16 02:53:48 +0200

    Finished html and css for programs section

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,27 @@
-const tabsContainer = document.querySelector(".tabs-conntainer");
-const tabsList = tabsContainer.querySelector(".tabs-title");
+const tabsContainer = document.querySelector(".tabs-container");
+const tabsList = document.querySelector(".tabs-title");
 const tabbuttons = tabsList.querySelectorAll("a");
-const tabPanels = tabsContainer.querySelectorAll(".tabs-panel-content > div");
+const tabPanels = document.querySelectorAll(".program-tab-content");
 
 tabbuttons.forEach((tab, index) => {
   if (index == 0) {
   } else {
-    tabPanels[index].setAttribute("hidden");
+    tabPanels[index].setAttribute("hidden", "");
   }
 });
+
+tabsContainer.addEventListener("click", (event) => {
+  const clicked = event.target.closest("a");
+  if (!clicked) return;
+  event.preventDefault();
+  switchTab(clicked);
+});
+
+const switchTab = (newTab) => {
+  const activeTabId = newTab.getAttribute("href");
+  const activeTab = tabsContainer.querySelector(activeTabId);
+  tabPanels.forEach((panel) => {
+    panel.setAttribute("hidden", true);
+  });
+  activeTab.removeAttribute("hidden", false);
+};
